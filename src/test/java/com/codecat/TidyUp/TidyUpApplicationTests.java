3b661fb1fbf8285c056a1c7038ec92656f296093
Alex Biedka <alexbiedka@gmail.com>
2025-12-08 23:10:45 +0100

    Spring setup

---
+++ src/test/java/com/codecat/TidyUp/TidyUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecat.TidyUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidyUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
