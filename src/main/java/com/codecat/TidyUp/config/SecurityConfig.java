cab33c34bcd670d6bac5bcf03f77312e09717534
Alex Biedka <alexbiedka@gmail.com>
2025-12-16 21:59:12 +0100

    Add Security config- password encoder

--- src/main/java/com/codecat/TidyUp/config/SecurityConfig.java
+++ src/main/java/com/codecat/TidyUp/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.codecat.TidyUp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+    }
 }
